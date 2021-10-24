65d672e6b1a465a790639bc3d6d7621892c8577c
J.Przydalski <przydal@gmail.com>
2021-10-24 10:28:23 +0200

    init moving road center line for tests

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const canvas = document.getElementById("myCanvas"),
     ctx = canvas.getContext("2d");
+
+const config = {
+    linesStartingPosition: -40
+}
+
+setInterval(generateInitialScreen, 50);
+
+function generateInitialScreen() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
+    ctx.fillStyle = "#5B5C66";
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    ctx.fillStyle = "white";
+
+    for (var i = 1; i < 1800; i++) {
+        ctx.fillRect(canvas.clientWidth / 2, config.linesStartingPosition + i * 45 - 1500, 10, 30);
+    }
+
+    config.linesStartingPosition += 10;
+
+}
